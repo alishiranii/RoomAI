1869de3b105bee99a8952a70d87a5c0570b0313c
alishiranii <alishiranii123@gmail.com>
2023-11-04 12:32:25 +0330

    added authinication to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider,SignInButton,UserButton,SignedIn,SignedOut } from "@clerk/nextjs";
+
 import "./globals.css";
 import Link from "next/link";
 
@@ -16,23 +18,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={
           inter.className +
-          " bg-slate-100 container mx-auto flex flex-col h-screen"
+          " bg-slate-200 container mx-auto flex flex-col h-screen"
         }
       >
         <header className="flex justify-between items-center p-5 lg:py-10">
           <Link href="/">
             <h3 className="font-bold text-4xl">RoomAI</h3>
           </Link>
-          <div className="cursor-pointer w-12 h-12 rounded-full bg-slate-500 flex items-center justify-center text-center font-semibold text-white text-xl outline outline-2 outline-rose-500">
-            A
-          </div>
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>  
+          <SignedOut>
+            <SignInButton/>
+          </SignedOut>
         </header>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
